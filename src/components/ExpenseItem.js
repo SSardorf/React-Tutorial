00abad2bdce6847a2dc35709a6fcec7435ba78e1
Simon Sardorf <SSardorf@gmail.com>
2021-07-27 22:23:09 +0300

    Make expenseItem dynamic using props
    This makes expenseItem act dynamic using props -> Will get more dynamic
    when additional functionality is added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,23 @@
-function ExpenseItem() {
-    return <div><div>Date</div><div><h2>Title</h2><div>Amount</div></div></div>;
+import "./ExpenseItem.css";
+
+function getDate() {
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0
+    var yyyy = today.getFullYear();
+    const expenseDate = dd + "/" + mm + "/" + yyyy;
+}
+
+function ExpenseItem(props) {
+    return (
+        <div className="expense-item">
+            <div>{props.date.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">DKK {props.amount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
