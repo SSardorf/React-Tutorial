a31103fdc71054bcbbc67f056c82b3de1a1ea53f
Simon Sardorf <SSardorf@gmail.com>
2021-07-28 00:00:57 +0300

    Upload solution to first assignment
    This commit adds the solution to the first assignment
    
    DO NOT MERGE

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
 import "./ExpenseItem.css";
-
-function getDate() {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, "0");
-    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0
-    var yyyy = today.getFullYear();
-    const expenseDate = dd + "/" + mm + "/" + yyyy;
-}
-
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
+    
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date ={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">DKK {props.amount}</div>
